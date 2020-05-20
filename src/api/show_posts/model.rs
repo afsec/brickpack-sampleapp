bdaac5b91d0d837438db36ae9d34247026d17cb0
Antonio Feitosa <antonio.cfc@gmail.com>
2020-05-20 14:02:54 -0300

    Fix framework upgrade issues

--- src/api/show_posts/model.rs
+++ src/api/show_posts/model.rs
@@ -1,10 +1,12 @@
 use brickpack::http_client::http_client;
 
 pub fn show_posts(body: Option<String>) -> Result<String, String> {
-    let token = "9admin9".to_string();
     // Request data from Concierge-db Server
     // https://github.com/afsec/concierge-db
+    let method = "GET".to_string();
     let url = "http://localhost:3341/api/posts/read-all".to_string();
-    let result = http_client(url, Some(token), None);
+    let token = "9admin9".to_string();
+
+    let result = http_client(method, url, Some(token), None);
     result
 }
--- src/api/show_users/model.rs
+++ src/api/show_users/model.rs
@@ -1,10 +1,11 @@
 use brickpack::http_client::http_client;
 
 pub fn show_users(body: Option<String>) -> Result<String, String> {
-    let token = "9admin9".to_string();
     // Request data from Concierge-db Server
     // https://github.com/afsec/concierge-db
+    let method = "GET".to_string();
     let url = "http://localhost:3341/api/users/read-all".to_string();
-    let result = http_client(url, Some(token), None);
+    let token = "9admin9".to_string();
+    let result = http_client(method, url, Some(token), None);
     result
 }
