a2ff5c3ad8f94cc5a33ae89744ae8f63a5c0e662
Antonio Feitosa <antonio.cfc@gmail.com>
2020-05-25 10:23:07 -0300

    Fix framework upgrade issues

--- src/api/groups/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod presenter;
-mod model;
-mod view;
--- src/api/groups/presenter.rs
+++
@@ -1,7 +0,0 @@
-use http_types::{Response, StatusCode};
-pub fn handler(body: String) -> Response {
-    println!("Handler groups ok!");
-    let mut response = Response::new(StatusCode::Ok);
-    response.set_body(body);
-    response
-}
