cdb982ac28011b74849fcae0c42f0d75ab11c01e
Antonio Feitosa <antonio.cfc@gmail.com>
2021-03-09 17:32:46 -0300

    Fix add_user

--- src/api/add_user/view.rs
+++ src/api/add_user/view.rs
@@ -3,13 +3,16 @@ use super::{AddUser, InternalMessage};
 use brickpack::endpoint::{Name, View};
 
 use serde_json::to_string as serde_json_to_string;
-use tide::{http::mime, Error as TideError, Response, StatusCode};
+use tide::{http::mime, Error as TideError, Response, StatusCode,prelude::Serialize};
+
+#[derive(Serialize)]
+struct ResponseBody {
+    status: String,
+    description: String,
+}
 
 impl View<InternalMessage> for AddUser {
     fn view(&self, result: Result<InternalMessage, TideError>) -> Response {
-        // let mut response = Response::builder(StatusCode::Ok)
-        //     .content_type(mime::JSON)
-        //     .build();
         match result {
             Ok(outcome) => {
                 tide::log::info!("ID created: {}", outcome.id);
@@ -23,17 +26,17 @@ impl View<InternalMessage> for AddUser {
                 tide::log::error!(r#"Endpoint [{}]: {}"#, self.name(), error);
                 #[cfg(debug_assertions)]
                 let error_message = error.to_string();
+
+
                 let mut response = Response::from(error);
+                response.set_content_type(mime::JSON);
                 response.insert_header("internal-error", &error_message);
-                let response_body = format!(
-                    r#"
-                {{ 
-                    status: "Error",
-                    description: "{error_message}"
-                }}"#,
-                    error_message = error_message
-                );
-                response.set_body(response_body);
+                let response_body = ResponseBody {
+                    status: "Error".into(),
+                    description: error_message,
+                };
+                let json_body = serde_json_to_string(&response_body).unwrap_or("".to_owned());
+                response.set_body(json_body);
                 response
             }
         }
